a5392542570bf46725ecc5eda1486394205803d9
Automagically <kylet9999@gmail.com>
2015-05-25 16:52:24 -0500

    Make a sprites folder and add a sprite named '0.png' to test

---
+++ Game.h
@@ -0,0 +1,6 @@
+#ifndef GAME_H
+
+static ALLEGRO_BITMAP** sprites;
+
+#define GAME_H
+#endif
--- main.cpp
+++ main.cpp
@@ -1,23 +1,44 @@
 #include <stdio.h>
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <string>
+#include "Game.h"
+using namespace std;
  
 int main(int argc, char **argv)
 {
    ALLEGRO_DISPLAY *display = NULL;
- 
+
    if(!al_init()) {
       fprintf(stderr, "failed to initialize allegro!\n");
       return -1;
    }
  
+   if(!al_init_image_addon()) {
+      fprintf(stderr, "failed to initialize image addon!\n");
+      return -1;
+   }
+
    display = al_create_display(704, 576);
    if(!display) {
       fprintf(stderr, "failed to create display!\n");
       return -1;
    }
- 
+
+   //load sprites
+   sprites = new ALLEGRO_BITMAP*[100];
+   for(int i = 0; i < 1; i++)
+   {
+	   string path = "Sprites/" + to_string((long long)i) + ".png";
+	   sprites[i] = al_load_bitmap(path.c_str());
+	   if(!sprites[i])
+		   return -2;
+   }
+
    al_clear_to_color(al_map_rgb(0,0,0));
  
+   al_draw_bitmap(sprites[0],0.0,0.0,0);
+
    al_flip_display();
  
    al_rest(10.0);
